a30269ca7355dc4cddd041dbd7afeb6da8182767
Gabriel Mesquita <gabrielmesquita0503@gmail.com>
2026-01-28 12:58:11 -0300

    chore: Remove unused test classes and configurations for reservation service

--- reservation-service/src/test/java/com/bookingmicroservice/reservationservice/TestReservationServiceApplication.java
+++
@@ -1,9 +0,0 @@
-package com.bookingmicroservice.reservationservice;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-@SpringBootApplication
-public class TestReservationServiceApplication {
-    // Test application without @EnableFeignClients
-}
